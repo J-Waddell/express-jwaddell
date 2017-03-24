bbbb745d78aba1490163c5aea794cf696c29c6f9
Jesse Waddell <jesse@Jesses-MacBook-Pro.local>
2017-03-24 11:30:51 -0500

    added in time request

--- express-hello-world.js
+++ express-hello-world.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const app = express()
+// console.log(express)
+
+const time = (req, res, next) => {
+    req.timeRequested = Date.now()
+    next()
+}
 
 app.get('/', (req, res) => {
     res.send('Hello, World!')
